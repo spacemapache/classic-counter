94cfe179e16077bf46e43463e4886721d53b5a4f
spacemapache <142768034+spacemapache@users.noreply.github.com>
2024-09-24 11:53:01 -0700

    added js code

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
-const add = document.getElementById("addBtn");
-const subtract = document.getElementById("subtractBtn");
+// define initial counter value
+let count = 0;
+
+// select elements
+const counterDisplay = document.getElementById("counter");
+const addNum = document.getElementById("addBtn");
+const subtractNum = document.getElementById("subtractBtn");
+
+//function to update display
+function updateDisplay() {
+  counterDisplay.textContent = count;
+}
+
+// function to add 1 to the counter
+function add() {
+  count++;
+  updateDisplay();
+}
+
+function subtract() {
+  count--;
+  updateDisplay();
+}
+
+updateDisplay();
